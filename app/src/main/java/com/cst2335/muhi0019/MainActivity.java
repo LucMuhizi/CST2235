2c9463831f65a072735e494da620362475fcd4e8
Luc Muhizi <muhi0019@algonquinlive.com>
2022-05-17 16:45:41 -0400

    Section 2

--- app/src/main/java/com/cst2335/muhi0019/MainActivity.java
+++ app/src/main/java/com/cst2335/muhi0019/MainActivity.java
@@ -3,12 +3,36 @@ package com.cst2335.muhi0019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button5;
+    private Switch switch2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button5 = findViewById(R.id.button5);
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toast_message = MainActivity.this.getResources().getString(R.string.toast_message)
+                Toast.makeText( MainActivity.this,  "Here is more information", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        switch2 = findViewById(R.id.switch2);
+        switch2.onChecked
+
     }
 }
